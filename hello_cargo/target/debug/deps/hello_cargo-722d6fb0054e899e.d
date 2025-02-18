e4eebfdf2cebe7973e086033cc5350e652bd2f60
Hazret Saçan <asklanhazret51@gmail.com>
2025-02-18 13:50:50 +0300

    Removed ignored files from tracking

--- hello_cargo/target/debug/deps/hello_cargo-722d6fb0054e899e.d
+++
@@ -1,5 +0,0 @@
-C:\Users\askla\OneDrive\Belgeler\Rust_Tutorials\hello_cargo\target\debug\deps\libhello_cargo-722d6fb0054e899e.rmeta: src\main.rs
-
-C:\Users\askla\OneDrive\Belgeler\Rust_Tutorials\hello_cargo\target\debug\deps\hello_cargo-722d6fb0054e899e.d: src\main.rs
-
-src\main.rs:
